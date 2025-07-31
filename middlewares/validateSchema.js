0f63f1e92c6448b6cc33455956ee299f2971111b
Cirooochen <cirochen0406@gmail.com>
2025-07-31 13:36:24 +0200

    updated validateSchema.js based on Daniel's changes

--- middlewares/validateSchema.js
+++ middlewares/validateSchema.js
@@ -1,16 +1,25 @@
-import { z } from "zod/v4";
+import z from "zod/v4";
 
 const validateSchema = (schema) => {
   return (req, res, next) => {
-    const result = schema.safeParse(req.body);
-    // console.log(result);
-    if (!result.success) {
-      console.log(result.error.issues);
-      /*       throw new Error(`Validation error: ${result.error}`);
-       */
+    const { data, error } = schema.safeParse(req.body);
+    // console.log(result); removed
 
-      throw new Error(`Validation error: ${z.prettifyError(result.error)}`);
+    if (error) {
+      const allErrors = error.issues.map((issue) => issue.message);
+      throw new Error(allErrors, { cause: 400 });
+
+      /*   PREVIOUS CODE  
+      if (!result.success) {
+      console.log(result.error);
+            throw new Error(`Validation error: ${result.error}`);
+        throw new Error(`Validation error: ${z.prettifyError(result.error)}`);
+    } */
+    } else {
+      req.sanitizedBody = data; // Creates a new property in req object with sanitized data from Zod
     }
+
+    next();
   };
 };
 
